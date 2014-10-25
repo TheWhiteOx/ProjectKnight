5a673aa6ef9199947269b63a794166b6598bafac
Raymond Luong <thewhiteoxinbox@gmail.com>
2014-10-24 17:44:58 -1000

    Created roomList template, added helper function  to access database

--- ProjectKnight.js
+++ ProjectKnight.js
@@ -5,18 +5,38 @@ Rooms = new Meteor.Collection('rooms');
 
 if (Meteor.isClient) {
 //write a helper on the formtemplate to check if userId matches builder Id
-Template.addRoom.isBuilder = function(){
-  var currentUser = Meteor.userId();
-  var builder = "8efHNS9m83oJcQCSx";
-  if (currentUser === builder){
-    return true;
-  } else{
-    return false;
-  }
-}
+  Template.addRoom.isBuilder = function(){
+    var currentUser = Meteor.userId();
+    var builder = "8efHNS9m83oJcQCSx";//need to move the ID to a server method, and call the ID from client.
+    if (currentUser === builder){
+      return true;
+    } else{
+      return false;
+    }
+  };
+
+  Template.roomList.listRooms = function(){
+    return "A List of Rooms should be here";
+  };
 
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Server stuff from here down
 
 if (Meteor.isServer) {
   
