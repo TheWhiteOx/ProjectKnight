d1abfb329e4959b2ce3e316c427e1cf2fd6e5c14
Raymond Luong <thewhiteoxinbox@gmail.com>
2014-10-24 16:22:46 -1000

    Added helper function to 'addRoom' template to hide or show 'addRoom' template

--- ProjectKnight.js
+++ ProjectKnight.js
@@ -1,10 +1,23 @@
-new Meteor.Collection('rooms');
+Rooms = new Meteor.Collection('rooms');
+
+
+
 
 if (Meteor.isClient) {
-
-
+//write a helper on the formtemplate to check if userId matches builder Id
+Template.addRoom.isBuilder = function(){
+  var currentUser = Meteor.userId();
+  var builder = "8efHNS9m83oJcQCSx";
+  if (currentUser === builder){
+    return true;
+  } else{
+    return false;
+  }
 }
+
+
+};
 
 if (Meteor.isServer) {
   
-}
+};
