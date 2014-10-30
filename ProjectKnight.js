414f852a660abdf76a2d802dbb8cea3d1fe942fb
Raymond Luong <thewhiteoxinbox@gmail.com>
2014-10-29 15:50:43 -1000

    Worked on function to map room database onto a 2d matrix

--- ProjectKnight.js
+++ ProjectKnight.js
@@ -8,7 +8,167 @@ Rooms = new Meteor.Collection('rooms');
 
 if (Meteor.isClient) {
 
-  //allows handlebars #each to take in objects
+ //playing around with grid mapper
+ //var mapRooms = function(){
+  //  var roomArray = Rooms.find().fetch();
+  //  console.log(roomArray[0]._id);
+  /*
+  1.  Mapper function takes in an array of room objects
+  2.  It sets the first element of the array to x y coordinates to 0,0
+  3.  Does that by pushing the first room to gridMap array with an object
+     with room ID as its name, and x y properties with values of 0 and 0
+    gridMap = {
+      "roomId1": {x: 0, y: 0};
+    }
+  4.  After setting the first room ID on the grid map, it pushes its current RoomID
+     to an array alreadyMapped.  = [room1,]
+
+     Also store CurrentRoom value to 0,0
+
+    Then the function goes thru the connected E W S E properties of room1 to check if its empty
+    if it has a room_id in it then it sets "newRoomId": {x: offset, y: offset} of each connected room.
+
+    THen marks current room as "spidered" by pushing it onto a spidered array.
+
+    then loops thru to see if connected rooms have bene spidered, if not
+
+     then run the function again, this time set the _id of the room to the X Y coords
+   
+// store all room objects into an roomArray
+   var roomArray = Rooms.find().fetch();
+//initialize a gridMap that stores roomID with key value pairs for x y coords
+   gridMap = {}
+//set the first item on the roomArray to 0,0 and put it in the gridMap obj
+   gridMap[roomArray[0]._id] = {
+                              roomTitle: roomArray[0].roomTitle,
+                              X: 0
+                              Y: 0
+                              Z: 0
+                            }
+//function to set coords of connected rooms below
+  roomToSpider(roomId)  //takes in array object such as roomArray[0]
+  var currentRoom = room
+  var currentRoomId = room._id;
+  var currentCoord = {
+                      x: gridMap[currentRoom].x,
+                      y: gridMap[currentRoom].y
+                     }
+  //maps all connected rooms to the gridMap with x y coordinates using currentCoord +- xy offset
+    for (var i = 0;i < roomArray;i++){
+      if (roomArray[i]._id === currentRoom){
+        if (roomArray[i].connectedWest !== ""){
+          gridMap.[roomArray[i].connectedWest].x = currentCoord.x -1;
+          gridMap.[roomArray[i].connectedWest].y = currentCord.y;  
+        }
+      }
+      if (roomArray[i]._id === currentRoom){
+        if (roomArray[i].connectedEast !== ""){
+          gridMap.[roomArray[i].connectedEast].x = currentCoord.x +1;
+          gridMap.[roomArray[i].connectedEast].y = currentCord.y;  
+        }
+      }
+      if (roomArray[i]._id === currentRoom){
+        if (roomArray[i].connectedNorth !== ""){
+          gridMap.[roomArray[i].connectedNorth].x = currentCoord.x;
+          gridMap.[roomArray[i].connectedNorth].y = currentCord.y +1;  
+        }
+      }
+      if (roomArray[i]._id === currentRoom){
+        if (roomArray[i].connectedSouth !== ""){
+          gridMap.[roomArray[i].connectedSouth].x = currentCoord.x ;
+          gridMap.[roomArray[i].connectedSouth].y = currentCord.y - 1;  
+        }
+      }
+    // after done mapping the connected rooms, set current room to Spidered.
+    var spidered = [currentRoom];
+
+
+    }
+ 
+
+
+
+
+
+  */
+
+
+ //};
+
+ Template.roomGrid.grid = function(){
+  
+  
+
+
+
+
+
+ return roomsArray;
+ }
+ 
+ Template.roomGrid.events({
+
+  'click input#renderGrid' : function(){
+
+    //construct an array of objects from the Rooms collection
+    var roomsArray = Rooms.find().fetch();
+    console.log(roomsArray);
+    //initialize a gridmap object that contains each roomID as a key and their x y coords as values
+    gridMap = {};
+
+    //insert the initial room into the gridMap
+    gridMap[roomsArray[0]._id] = {
+      roomTitle: roomsArray[0].roomTitle,
+      x: 0,
+      y: 0,
+    };
+    console.log(gridMap);
+
+    //function to scan attached rooms and push them onto the gridMap obj.
+    var spiderThisRoom = function(roomId){
+          var currentRoomId = roomId;
+          var currentRoomObj = (function(){
+                                for (var i = 0;i<roomsArray.length;i++){
+                                  if (currentRoomId === roomsArray[i]._id){
+                                    return roomsArray[i];
+                                  };
+                                };
+                               })();
+
+          var currentCoords = {x: gridMap[currentRoomId].x, y: gridMap[currentRoomId].y};
+        
+          if (currentRoomObj.connectedSouth !== ""){
+              gridMap[currentRoomObj.connectedSouth] = {x: currentCoords.x, y: currentCoords.y - 1};
+             
+
+          } else if (currentRoomObj.connectedNorth !== ""){
+              gridMap[currentRoomObj.connectedNorth] = {x: currentCoords.x, y: currentCoords.y + 1};
+              
+
+          } else if (currentRoomObj.connectedWest !== ""){
+              gridMap[currentRoomObj.connectedWest] = {x: currentCoords.x - 1, y: currentCoords.y};
+             
+
+          } else if (currentRoomObj.connectedEast !== ""){
+              gridMap[currentRoomObj.connectedEast] = {x: currentCoords.x + 2, y: currentCoords.y};
+              
+
+          };
+
+             console.log(gridMap);
+    }
+
+     spiderThisRoom("mfFLnAARmf3H7d79H");
+
+ 
+  }
+
+
+ });
+
+
+
+  //allows handlebars #each to take in objects and convert them into arrays
   Handlebars.registerHelper('arrayify', function(obj){
     result = [];
     for (var key in obj) result.push({name:key,value:obj[key]});
@@ -109,7 +269,7 @@ Template.roomList.isBuilder = function(){//helper function to validate if user h
   };
 
 
-  //lists players in the room and their messages
+  //lists players in the room and their emote messages
   Template.roomContents.listPlayers = function(){
     var playerMap = {};
     var currentUser = Meteor.userId();
@@ -216,8 +376,7 @@ mic.onresult = function (intent, entities) {
              if (!roomUp){return console.log('Alas, you cannot go that way.');};
              new Audio('/audio/forest_footsteps.mp3').play();
              Meteor.users.update({_id: currentUser},{$set:{'profile.roomIn': roomUp}});
-     moveTo(currentUser,roomIn,roomUp);
-
+             moveTo(currentUser,roomIn,roomUp);
         } else if (intent === "move_down"){
            var currentUser = Meteor.userId();
            var roomIn = Meteor.users.findOne({_id: currentUser},{'profile.roomIn': 1}).profile.roomIn;
@@ -227,6 +386,13 @@ mic.onresult = function (intent, entities) {
            new Audio('/audio/forest_footsteps.mp3').play();
            Meteor.users.update({_id: currentUser},{$set:{'profile.roomIn': roomDown}});
            moveTo(currentUser,roomIn,roomDown);
+        } else if (intent === "recall_origin") {
+           var currentUser = Meteor.userId();
+           roomIn = Meteor.users.findOne({_id: currentUser},{'profile.roomIn': 1}).profile.roomIn;
+           var roomOrigin = Rooms.findOne({roomTitle: 'Origin of Light'},{_id: 1})._id;
+           new Audio('/audio/recall_origin.mp3').play();
+           Meteor.users.update({_id: currentUser},{$set:{'profile.roomIn': roomOrigin}});
+           moveTo(currentUser,roomIn,roomOrigin);
         }
 
 
@@ -472,6 +638,13 @@ Template.chatBox.events({
     },10000);
   }
 });
+
+
+
+
+var roomArray = Rooms.find().fetch();
+console.log(roomArray);
+
 
 
 };
